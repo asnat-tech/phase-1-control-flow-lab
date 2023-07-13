fcdc24afb626e6dbcded03f3037e465dc438082e
asnat <asnatkemuma@gmail.com>
2023-07-14 00:14:19 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-function scuberGreetingForFeet(Price){
-if ( 'distance=>400 feet') 
-{return scuberGreetingForFeet(199) ('This one is on me!')
-}
-else if ( 'distance<2000')
-  {return scuberGreetingForFeet(2001) ('I will gladly take your thirty bucks.')
-}
-else  ( 'distance<2500')
-{return scuberGreetingForFeet(2501) ('No can do')}
+function scuberGreetingForFeet(distance){
+if ('distance'<=400 )
+return  ("This one is on me!")
+
+else if ( 'distance<2000' && 'distance<=2500')
+  return  ('I will gladly take your thirty bucks.')
+
+else 
+return ('No can do')
 }
 
-function ternaryCheckCity(City){
-if ( 'city is NYC')
-{ return ternaryCheckCity('NYC') ('Ok, sounds good.')
+function ternaryCheckCity(destination){
+if ('destination = NYC')
+return
+('Ok, sounds good.')
+
+('No go.')
+else ('No go.')
 }
-else (ternaryCheckCity('Pittsburgh'))('No go.')}
 
 
-function switchOnCharmFromTip(Tip){
+function switchOnCharmFromTip(tip){
 if ('the tip is generous')
-return (switchOnCharmFromTip('generous'))('Thank you so much.')
+return ('Thank you so much.')
 
 else if ('the tip is not as generous')
-return ( (switchOnCharmFromTip)('not as generous'))
+return ('not as generous')
 ('Thank you.')
 
-else  ('anything else') 
-return (switchOnCharmFromTip('thanks for everything')) ('Bye.')}
+else  "Bye"}
