c337da713170c6141f547a3a78a6fa2f72f28659
asnat <asnatkemuma@gmail.com>
2023-07-13 19:06:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Price){
+if ( 'distance=>400 feet') 
+{return scuberGreetingForFeet(199) ('This one is on me!')
+}
+else if ( 'distance<2000')
+  {return scuberGreetingForFeet(2001) ('I will gladly take your thirty bucks.')
+}
+else  ( 'distance<2500')
+{return scuberGreetingForFeet(2501) ('No can do')}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+if ( 'city is NYC')
+{ return ternaryCheckCity('NYC') ('Ok, sounds good.')
 }
+else (ternaryCheckCity('Pittsburgh'))('No go.')}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+function switchOnCharmFromTip(Tip){
+if ('the tip is generous')
+return (switchOnCharmFromTip('generous'))('Thank you so much.')
+
+else if ('the tip is not as generous')
+return ( (switchOnCharmFromTip)('not as generous'))
+('Thank you.')
+
+else  ('anything else') 
+return (switchOnCharmFromTip('thanks for everything')) ('Bye.')}
